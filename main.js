374e871afa66b234b8a53ed538b911c26380b991
FlyingColors02 <shraduborse97@gmail.com>
2020-01-20 13:05:04 +0530

    #shraddha borse: added if else statements

---
+++ main.js
@@ -0,0 +1,67 @@
+//if else statement
+
+let numValue1=10;
+let numValue2=20;
+let strValue1="java";
+let strValue2="10";
+let strValue3="java";
+let strValue4="15";
+
+//comparing two numbers
+if(numValue1==numValue2)
+{
+    console.log("matched");
+}
+else
+{
+    console.log("not matched");
+}
+
+
+//comparing two string values
+if(strValue1==strValue2)
+{
+    console.log("matched");
+}
+else
+{
+    console.log("not matched");
+}
+
+if(strValue1==strValue3)
+{
+    console.log("matched");
+}
+else
+{
+    console.log("not matched");
+}
+
+
+//comparing number and string
+if(numValue1==strValue1)
+{
+    console.log("matched");
+}
+else
+{
+    console.log("not matched");
+}
+
+if(numValue1==strValue2)
+{
+    console.log("matched");
+}
+else
+{
+    console.log("not matched");
+}
+
+if(numValue1==strValue4)
+{
+    console.log("matched");
+}
+else
+{
+    console.log("not matched");
+}
